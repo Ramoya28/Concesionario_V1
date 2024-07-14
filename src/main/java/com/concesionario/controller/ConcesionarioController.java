6a6251b46a984328a69386d9a9b76d6ee8b0418f
Ramoya28 <randallmontenegro87@gmail.com>
2024-07-14 16:42:43 -0600

    Se fusionan las funciones de registrar usuario e inicio de sesion
    El inicio de sesiones queda con correo y contraseña unicamente

--- src/main/java/com/concesionario/controller/ConcesionarioController.java
+++ src/main/java/com/concesionario/controller/ConcesionarioController.java
@@ -1,7 +1,9 @@
 package com.concesionario.controller;
 
 import com.concesionario.domain.Categoria;
+import com.concesionario.domain.Usuario;
 import com.concesionario.service.CategoriaService;
+import com.concesionario.service.UsuarioService;
 import com.concesionario.service.impl.FirebaseStorageServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +19,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @RequestMapping("/marcas") //(marcas/listado)
 public class ConcesionarioController {
-    
+
     @Autowired
     private CategoriaService marcasService;
+
+    @Autowired
+    private UsuarioService usuarioService;
 
     @GetMapping("/listado")
     public String inicio(Model model) {
@@ -28,7 +33,36 @@ public class ConcesionarioController {
         model.addAttribute("totalCategorias", marcass.size());
         return "/marcas/listado";
     }
-    
+
+    @GetMapping("/registro")
+    public String inicioU(Model model) {
+        var usuarios = usuarioService.getInformacion(0);
+        model.addAttribute("marcass", usuarios);
+        model.addAttribute("totalCategorias", usuarios.size());
+        return "/marcas/registro";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/marcas/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("correo") String correo,
+            @RequestParam("contrasena") String contrasena,
+            Model model) {
+        Usuario usuario = usuarioService.validateUser(correo, contrasena);
+        if (usuario != null) {
+            // Lógica después de iniciar sesión con éxito
+            model.addAttribute("usuario", usuario);
+            return "redirect:/marcas/listado"; // Cambia a la vista que desees mostrar después del login
+        } else {
+            // Lógica para manejo de error de login
+            model.addAttribute("error", "Correo o contraseña incorrectos");
+            return "/marcas/login";
+        }
+    }
+
     @GetMapping("/nuevo")
     public String marcasNuevo(Categoria marcas) {
         return "/marcas/modifica";
@@ -36,16 +70,16 @@ public class ConcesionarioController {
 
     @Autowired
     private FirebaseStorageServiceImpl firebaseStorageService;
-    
+
     @PostMapping("/guardar")
     public String marcasGuardar(Categoria marcas,
-            @RequestParam("imagenFile") MultipartFile imagenFile) {        
+            @RequestParam("imagenFile") MultipartFile imagenFile) {
         if (!imagenFile.isEmpty()) {
             marcasService.save(marcas);
             marcas.setRutaImagen(
                     firebaseStorageService.cargaImagen(
-                            imagenFile, 
-                            "marcas", 
+                            imagenFile,
+                            "marcas",
                             marcas.getIdCategoria()));
         }
         marcasService.save(marcas);
@@ -64,5 +98,29 @@ public class ConcesionarioController {
         model.addAttribute("marcas", marcas);
         return "/marcas/modifica";
     }
-    
+
+    /*--------USUARIO-------*/
+    @GetMapping("/usuario")
+    public String registroUsuario(Usuario usuario) {
+        return "/marcas/usuario";
+    }
+
+    @GetMapping("/usuario/nuevo")
+    public String formualarioRegistro(Model model, Usuario usuario) {
+        model.addAttribute("usuario", usuario);
+        return "usuarioNuevo";
+    }
+
+    @PostMapping("marcas/guardarUsuario")
+    public String guardarUsuario(Usuario usuario) {
+        usuarioService.save(usuario);
+        return "redirect:/marcas/registro";
+    }
+
+    @GetMapping("/eliminar/{idUsuario}")
+    public String eliminarUsuario(Usuario usuario) {
+        usuarioService.delete(usuario);
+        return "redirect:/marcas/usuario";
+    }
+
 }
